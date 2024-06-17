9987050e9ddedc84ee444a4df1a233d4b95cd002
Adão Araujo <adaoma11@hotmail.com>
2024-06-17 14:43:56 -0300

    feat: add authentication and session features

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../../assets/img/logo.png";
 
+// contexts
+import { Context } from "../../context/UserContext";
+
 function Navbar() {
+    const { authenticated, logout } = useContext(Context);
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar_logo}>
@@ -13,12 +18,21 @@ function Navbar() {
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
-                    <Link to="/login">Entrar</Link>
-                </li>
-                <li>
-                    <Link to="/register">Cadastrar</Link>
-                </li>
+
+                {authenticated ? (
+                    <>
+                        <li onClick={logout}>Logout</li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link to="/login">Entrar</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Cadastrar</Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     );
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,7 +4,18 @@ import { useNavigate } from "react-router-dom";
 import useFlashMessage from "./useFlashMessage";
 
 export default function useAuth() {
+    const [authenticated, setAuthenticated] = useState(false);
     const { setFlashMessage } = useFlashMessage();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+            setAuthenticated(true);
+        }
+    }, []);
 
     async function register(user) {
         let msgText = "Cadastro realizado com sucesso";
@@ -14,6 +25,8 @@ export default function useAuth() {
             const data = await api
                 .post("/users/register", user)
                 .then((response) => response.data);
+
+            await authUser(data);
         } catch (err) {
             msgText = err.response.data.message;
             msgType = "error";
@@ -21,5 +34,23 @@ export default function useAuth() {
         setFlashMessage(msgText, msgType);
     }
 
-    return { register };
+    async function authUser(data) {
+        setAuthenticated(true);
+        localStorage.setItem("token", JSON.stringify(data.token));
+        navigate("/");
+    }
+
+    function logout() {
+        const msgText = "Logout realizado com sucesso";
+        const msgType = "success";
+
+        setAuthenticated(false);
+        localStorage.removeItem("token");
+        api.defaults.headers.Authorization = undefined;
+        navigate("/");
+
+        setFlashMessage(msgText, msgType);
+    }
+
+    return { authenticated, register, logout };
 }
